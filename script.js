ae223c064101fadfbe6585bed1b1f59261a074a4
katian28 <nkurunzizakati@gmail.com>
2024-09-11 16:14:00 +0800

    updates

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var menu = document.getElementById("menu");
 sideNav.style.right = "-250px";
 
 menuBtn.onclick = function () {
-  if (sideNav.style.right == "-250px") {
+  if (sideNav.style.right === "" || sideNav.style.right === "-250px") {
     sideNav.style.right = "0";
     menu.src = "/images/close.png";
   } else {
